838dc9365d5de79794c9a23c8805d533771451c8
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-10-28 20:15:52 -0400

    take photo completed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -44,7 +44,7 @@ dependencies {
     val navigationVersion = "2.6.0"
     val lifecycle_version = "2.3.1"
 
-    implementation("com.google.firebase:firebase-firestore-ktx:24.9.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -56,13 +56,12 @@ dependencies {
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    implementation("com.google.firebase:firebase-auth:22.2.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.firebase:firebase-firestore:24.9.0")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
+   
 
     val camerax_version = "1.2.2"
     implementation("androidx.camera:camera-core:${camerax_version}")
